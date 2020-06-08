0f293437a9f996c0179012e2bf82a69640e5f65f
rohit163 <rohitphatarpekar@gmail.com>
2020-06-09 04:01:12 +0530

    final

--- lib/drawerScreen.dart
+++ lib/drawerScreen.dart
@@ -12,6 +12,89 @@ class _DrawerScreenState extends State<DrawerScreen> {
   Widget build(BuildContext context) {
     return Container(
       color: primaryGreen,
+      padding: EdgeInsets.only(top: 70, bottom: 60, left: 20,),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              CircleAvatar(),
+              SizedBox(
+                width: 18,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Rohit Phatarpekar',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    'Active Status',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              )
+            ],
+          ),
+          Column(
+            children: drawerItems
+                .map((element) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                        children: <Widget>[
+                          Icon(
+                            element['icon'],
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                          SizedBox(width: 10,),
+                          Text(
+                            element['title'],
+                            style: TextStyle(
+                                color: Colors.white, fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                ))
+                .toList(),
+          ),
+          Row(
+            children: <Widget>[
+              Icon(
+                Icons.settings,
+                color: Colors.white,
+              ),
+              SizedBox(
+                width: 12,
+              ),
+              Text(
+                'Settings',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                width: 12,
+              ),
+              Container(
+                width: 2,
+                height: 22,
+                color: Colors.white,
+              ),
+              SizedBox(
+                width: 12,
+              ),
+              Text(
+                'Log Out',
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
